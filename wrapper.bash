3d8e2dbe227743a063e1f61dc127b93fb6232a20
aaronryank <rkcoder2002@gmail.com>
2018-03-07 02:20:03 +0000

    Remove arguments and stuff

--- wrapper.bash
+++ wrapper.bash
@@ -4,7 +4,7 @@ mcc="$(cd "$(dirname -- "$(readlink -e -- "$0")")"; pwd)/MachineCode"
 src="$(mktemp ./tmpXXXXX.c)"
 bin="$(mktemp ./tmpXXXXX)"
 
-"$mcc" "$1" "$(cat)" "${@:2}" > "$src"
+"$mcc" "$1" > "$src"
 gcc -w -o "$bin" "$src"
 "$bin"
 
