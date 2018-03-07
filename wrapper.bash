c78f20e5478591a268afa7823cb17a63650600e0
MD XF <aaronryank@gmail.com>
2018-03-06 17:10:46 -0800

    Update wrapper.bash

--- wrapper.bash
+++ wrapper.bash
@@ -1,3 +1,9 @@
-./MachineCode .code.tio `cat .input.tio` "$@" > tmp.c
-gcc tmp.c -w
-./a.out
+#!/usr/bin/env bash
+
+mcc="$(cd "$(dirname -- "$(readlink -e -- "$0")")"; pwd)/MachineCode"
+src="$(mktemp ./tmpXXXXX.c)"
+bin="$(mktemp ./tmpXXXXX)"
+
+"$mcc" "$1" "$(cat)" "${@:2}" > "$src"
+gcc -w -o "$bin" "$src"
+"$bin"
